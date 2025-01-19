61acb905f5a47813a99194ff927e5868525603b1
Kyamran <kyamranr@gmail.com>
2025-01-19 14:20:52 -0500

    added userRoutes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const userRoutes = require("./routes/userRoutes");
 
 app.use(cors());
 app.use(express.json()); // Parse JSON requests
@@ -9,5 +10,7 @@ app.use(express.json()); // Parse JSON requests
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+
+app.use("/users", userRoutes);
 
 module.exports = app;
