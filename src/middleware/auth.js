1dd3422c34d8eac0005ae017b4d824027baba8fc
Kyamran <kyamranr@gmail.com>
2025-01-23 11:17:00 -0600

    added ensure admin function

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,6 +2,19 @@ const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = require("../config");
 const { UnauthorizedError } = require("../expressError");
 
+/** Middleware: Authenticate user by verifying JWT token */
+function authenticateJWT(req, res, next) {
+  try {
+    const token = req.headers.authorization?.split(" ")[1]; // Bearer <token>
+    if (token) {
+      res.locals.user = jwt.verify(token, SECRET_KEY);
+    }
+    return next();
+  } catch (err) {
+    return next(); // If no token, proceed without user
+  }
+}
+
 /** Middleware to ensure user is logged in */
 function ensureLoggedIn(req, res, next) {
   try {
@@ -27,4 +40,22 @@ function ensureCorrectUser(req, res, next) {
   }
 }
 
-module.exports = { ensureLoggedIn, ensureCorrectUser };
+/** Middleware to ensure the user is an admin */
+function ensureAdmin(req, res, next) {
+  try {
+    const user = res.locals.user;
+    if (user && user.role === "admin") {
+      return next();
+    }
+    throw new UnauthorizedError("Unauthorized: Admin privileges required.");
+  } catch (error) {
+    return next(error);
+  }
+}
+
+module.exports = {
+  authenticateJWT,
+  ensureLoggedIn,
+  ensureCorrectUser,
+  ensureAdmin,
+};
