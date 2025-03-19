f1ee6ff5bd779e0647508fad67130e5b1a63aa80
Kyamran <kyamranr@gmail.com>
2025-03-19 19:14:47 -0400

    added \i ahiska-schema.sql

--- src/ahiska.sql
+++ src/ahiska.sql
@@ -3,7 +3,11 @@ DROP DATABASE ahiskaDB;
 CREATE DATABASE ahiskaDB;
 \connect ahiskaDB
 
+\i ahiska-schema.sql
+
 -- Delete and recreate ahiskaDB_test
 DROP DATABASE ahiskaDB_test;
 CREATE DATABASE ahiskaDB_test;
 \connect ahiskaDB_test
+
+\i ahiska-schema.sql
