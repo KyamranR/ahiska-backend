bdb2c94ab386d4f3b2c611e3e1542e08584fee76
Kyamran <kyamranr@gmail.com>
2025-04-03 19:23:36 -0400

    renamed date and time

--- src/ahiska-schema.sql
+++ src/ahiska-schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE events (
     title VARCHAR(255) NOT NULL,
     description TEXT,
     event_date DATE NOT NULL,
-    event_time TIME NOT NULL,
+    event_time TEXT NOT NULL,
     location VARCHAR(255) NOT NULL,
     created_by INT REFERENCES users(id) ON DELETE CASCADE
 );
